fc6a86de2c43b0e9712f3bbbeb629902eb46062d
Rickin <rickin.shah17403@gmail.com>
2024-05-29 19:58:50 +0530

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.o.tabstop = 4
+vim.b.autoformat = false
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
